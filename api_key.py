c0eea3eac247f3d95331a6035a3bf312972a550c
Aditya Nayak <93430846+Adinyk03@users.noreply.github.com>
2023-11-09 13:59:33 +0000

    Update api_key.py

--- api_key.py
+++ api_key.py
@@ -1,3 +1,6 @@
 
+client_var = "mongodb://localhost:27017/"
+db_var = "chat_hist"
+collection_var = "chats"
 
-key = "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"
+key = "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"
