3735823f4d63d326598fe0acddf278e803e8d28c
Aditya Nayak <93430846+Adinyk03@users.noreply.github.com>
2023-11-09 13:58:06 +0000

    Update main.py
    we also added session id to the code such that when a new conversation is started by the user then the session id is saved in the conversation history in the mongodb

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import openai
 from flask import Flask, request, jsonify
-from mongo_client import chat_finder, chat_exist, new_chat
-from api_key import key
+from functions import chat_finder, chat_exist, new_chat, generate_id
+from config import key
 
 app = Flask(__name__)
 
@@ -10,19 +10,28 @@ openai.api_key = key
 
 @app.route("/add", methods=['POST'])
 def addition():
+    object_id = generate_id()
     user_input = request.json
     sys_prompt = user_input["prompt"]
     user_prompt = user_input["user_id"]
+    cont_convo = user_input["same_conversation"]
 
     conversation_history = []
     chat_document = chat_finder(user_prompt)
 
-    if chat_document is not None:
-        response1 = chat_exist(chat_document, sys_prompt)
-        return jsonify({"response": response1})
-    else:
-        response1, chat_id = new_chat(user_prompt, sys_prompt, conversation_history)
+    if chat_document is None:
+        session_id = generate_id()  # Generate a new session ID for new conversations
+        response1, chat_id = new_chat(object_id, user_prompt, sys_prompt, conversation_history, session_id)
         return jsonify({"response": response1, "chat_id": chat_id})
+    else:
+        if cont_convo.lower() == "yes":
+            # No new session ID is generated for continuing old conversations
+            response1 = chat_exist(chat_document, sys_prompt)
+            return jsonify({"response": response1})
+        else:
+            session_id = generate_id()  # Generate a new session ID for new conversations
+            response1 = chat_exist(chat_document, sys_prompt, session_id)  # Pass session_id here
+            return jsonify({"response": response1})
 
 
 if __name__ == '__main__':
